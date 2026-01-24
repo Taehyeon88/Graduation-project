7d24a018eae2787e61ee3ad455acf3bf89007534
Taehyeon88 <Taehyeon5376@naver.com>
2026-01-24 18:27:11 +0900

    [Pro Feat] 2D -> 3D 개발환경으로 변경

--- Game_project/Assets/Scripts/SO/Database/SOKeyType.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public static class SOKeyType
-{
-    public static string[] types = new string[]{CardAbilityStats,CardStats};
-    public static string CardAbilityStats = "CardAbilityStats";
-    public static string CardStats = "CardStats";
-}
