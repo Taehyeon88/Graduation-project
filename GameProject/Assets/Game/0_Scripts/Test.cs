0b9dead1f48900dfe4ea90d63475ea8f39acbfa0
Taehyeon88 <Taehyeon5376@naver.com>
2026-06-22 20:30:25 +0900

    [Pro Feat] 기말 발표용 맵 데이터 생성 및 적용

--- GameProject/Assets/Game/0_Scripts/Test.cs
+++ GameProject/Assets/Game/0_Scripts/Test.cs
@@ -1,17 +1,32 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class Test : MonoBehaviour
 {
     private void Update()
     {
+        //튜토리얼 넘기기
         if (Input.GetKeyDown(KeyCode.T))
         {
-            GameClearGA gameClearGA = new GameClearGA();
-            ActionSystem.Instance.Perform(gameClearGA);
+            if (TutorialSystem.Instance.IsTutorialing)
+                GameSystem.Instance.StartFromScratch();
+        }
+
+        //모든 몬스터 처치
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (EnemySystem.Instance.Enemise.Count <= 0) return;
+
+            var targets = new List<CombatantView>();
+            foreach(var enemy in EnemySystem.Instance.Enemise)
+                targets.Add(enemy);
+            DealDamageGA dealDamageGA = new(100, targets, HeroSystem.Instance.HeroView);
+            ActionSystem.Instance.Perform(dealDamageGA);
         }
     }
 }
